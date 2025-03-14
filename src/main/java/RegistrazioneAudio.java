e0fce69b93c73c0955694e34315fe996850a3208
Luca Zuin <zuin.luca1111@gmail.com>
2025-03-14 13:40:06 +0100

    commit

--- src/main/java/RegistrazioneAudio.java
+++ src/main/java/RegistrazioneAudio.java
@@ -1,2 +1,28 @@
-public class RegistrazioneAudio {
+// classe RegistrazioneAudio estende ElementoMultimediale, variabili durata e volume (int) e un costruttore per assegnare durata e volume
+public class RegistrazioneAudio extends ElementoMultimediale {
+    private int durata, volume;
+
+    public RegistrazioneAudio(String titolo, int durata, int volume) {
+        super(titolo);
+        this.durata = durata;
+        this.volume = volume;
+    }
+
+    public void aumentaVolume() {
+        volume += 10;
+    }
+
+    public void diminuisciVolume() {
+        volume -= 10;
+    }
+
+    // metodo play() (durata, volume)
+    public void play() {
+        System.out.println("Durata: " + durata + ", Volume: " + volume);
+    }
+
+    @Override
+    public void esegui() {
+
+    }
 }
--- src/main/java/Video.java
+++ src/main/java/Video.java
@@ -1,2 +1,38 @@
-public class Video {
+// classe Video estende ElementoMultimediale, variabili luminosità, volume e durata (int) e un costruttore per assegnare luminosità, volume e durata
+public class Video extends ElementoMultimediale {
+    private int luminosita;
+    private int volume;
+    private int durata;
+
+    public Video(String titolo, int luminosita, int volume, int durata) {
+        super(titolo);
+        this.luminosita = luminosita;
+        this.volume = volume;
+        this.durata = durata;
+    }
+
+    public void aumentaLuminosita() {
+        luminosita += 10;
+    }
+
+    public void diminuisciLuminosita() {
+        luminosita -= 10;
+    }
+
+    public void aumentaVolume() {
+        volume += 10;
+    }
+
+    public void diminuisciVolume() {
+        volume -= 10;
+    }
+
+    public void play() {
+        System.out.println("Durata: " + durata + ", Volume: " + volume + ", Luminosità: " + luminosita);
+    }
+
+    @Override
+    public void esegui() {
+        play();
+    }
 }
