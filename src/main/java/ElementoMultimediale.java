e9cb0e5aa67f1b90b82df160b929a6ac0e17b420
Luca Zuin <zuin.luca1111@gmail.com>
2025-03-14 12:59:10 +0100

    commit

--- src/main/java/ElementoMultimediale.java
+++ src/main/java/ElementoMultimediale.java
@@ -1,2 +1,11 @@
-public class ElementoMultimediale
+// classe astratta ElementoMultimediale con titolo (string) e un costruttore per assegnare il titolo
+public abstract class ElementoMultimediale {
+    protected String titolo;
+
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+    }
+
+    // metodo astratto esegui() (da implementare nelle sottoclassi)
+    public abstract void esegui();
 }
--- src/main/java/Immagine.java
+++ src/main/java/Immagine.java
@@ -1,2 +1,28 @@
-public class Immagine {
+// classe Immagine che estende ElementoMultimediale, variabile luminosità (int) e un costruttore per assegnare luminosità
+public class Immagine extends ElementoMultimediale {
+    private int luminosita;
+
+    public Immagine(String titolo, int luminosita) {
+        super(titolo);
+        this.luminosita = luminosita;
+    }
+
+    public void aumentaLuminosita() {
+        luminosita += 10;
+    }
+
+    public void diminuisciLuminosita() {
+        luminosita -= 10;
+    }
+
+    // show() (luminosità)
+    public void show() {
+        System.out.println("Luminosità: " + luminosita);
+    }
+
+    // implementa esegui() richiamando show(), utilizza lombok
+    @Override
+    public void esegui() {
+        show();
+    }
 }
